5ee67aeddf9862953f0306e18ddfe66c3490abb0
robmux <robinsonmu232@gmail.com>
2024-03-13 23:52:59 -0500

    Add entry endpoints

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,8 +13,32 @@ func main() {
 		c.String(200, "Hello, Gin!")
 	})
 
+	r.POST("/load-transactions", func(c *gin.Context) {
+		c.String(200, "loading transactions")
+	})
+
+	r.POST("/notify", makeHTTPHandler(notifyUsers))
+
 	err := r.Run(":3000")
 	if err != nil {
 		fmt.Println("error ", err.Error())
 	}
+}
+
+type handler func(ctx *gin.Context) error
+
+func makeHTTPHandler(h handler) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		err := h(ctx)
+		if err != nil {
+			ctx.String(500, err.Error())
+		}
+
+		// Nothing here, because h should have already sent the response
+	}
+}
+
+func notifyUsers(ctx *gin.Context) error {
+	ctx.String(200, "users notified")
+	return nil
 }
